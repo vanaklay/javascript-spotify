72a990091e33e2a3d44541ff2b4afea608df8069
umiis1008 <umair@hotmail.fr>
2021-06-01 14:40:28 +0200

    dyhgfyh

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("ceci est le script");
+// select buttons
+
+// selections des boutons
+const buttons = document.querySelectorAll("button")
+
+// assignation des boutons
+const firstSongButton = document.getElementById('btn-add-1')
+const secondSongButton = document.getElementById('btn-add-2')
+
+firstSongButton.addEventListener('click', () => {
+    
+});
